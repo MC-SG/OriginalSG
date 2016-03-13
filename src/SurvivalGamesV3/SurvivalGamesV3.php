fee568d314680d740d4cbf2d7a8aac67478b4679
Jake <jakehcasto@aol.com>
2016-03-12 21:12:38 -0500

    EconomyAPI support!
    Should give winner 1K after winning a match, have not tested it yet.

--- src/SurvivalGamesV3/SurvivalGamesV3.php
+++ src/SurvivalGamesV3/SurvivalGamesV3.php
@@ -404,6 +404,7 @@ class GameSender extends PluginTask {
 										$pl->sendMessage($this->prefix . TextFormat::GREEN . "You won!");
 										$pl->getInventory()->clearAll();
 										$pl->removeAllEffects();
+										$p1->economy->addMoney($p1, 1000);
 										$pl->setNameTag($pl->getName());
 										$spawn = $this->plugin->getServer()->getDefaultLevel()->getSafeSpawn();
 										$this->plugin->getServer()->getDefaultLevel()->loadChunk($spawn->getX(), $spawn->getZ());
