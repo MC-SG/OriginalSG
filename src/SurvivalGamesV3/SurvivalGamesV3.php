c190b06f8aa6aaf71e9d1b4a1d33986535fb652d
Jake <jakehcasto@aol.com>
2016-03-18 12:48:26 -0400

    Update SurvivalGamesV3.php

--- src/SurvivalGamesV3/SurvivalGamesV3.php
+++ src/SurvivalGamesV3/SurvivalGamesV3.php
@@ -422,7 +422,7 @@ class GameSender extends PluginTask {
 										$pl->sendTip($this->prefix . TextFormat::GREEN . "You won!");
 										$pl->getInventory()->clearAll();
 										$pl->removeAllEffects();
-							                        $this->economy->addMoney($p1, 1000, false, "SurvivalGamesV3")
+							                        $p1->economy->addMoney($p1, 1000, false, "SurvivalGamesV3")
 										$spawn = $this->plugin->getServer()->getDefaultLevel()->getSafeSpawn();
 										$this->plugin->getServer()->getDefaultLevel()->loadChunk($spawn->getX(), $spawn->getZ());
 										$pl->teleport($spawn,0,0);
