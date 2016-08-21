58981bcd881f777e68fdf6fcc30f224758a12f16
Jake <jakehcasto@aol.com>
2016-08-20 20:15:40 -0400

    Create StatsCommand.php

---
+++ src/ImagicalGamer/SurvivalGames/Commands/StatsCommand.php
@@ -0,0 +1,38 @@
+<?php
+namespace ImagicalGamer\SurvivalGames\Commands;
+
+use ImagicalGamer\SurvivalGames\BaseFiles\BaseCommand;
+use ImagicalGamer\SurvivalGames\Main;
+use ImagicalGamer\SurvivalGames\Tasks\Stats\TopStatsTask;
+use pocketmine\Player;
+use pocketmine\command\CommandSender;
+
+use pocketmine\utils\TextFormat as C;
+
+class StatsCommand extends BaseCommand{
+
+    private $plugin;
+
+    public function __construct($name, Main $plugin){
+        parent::__construct("sgstats", $plugin);
+        $this->plugin = $plugin;
+        $this->setDescription("SurvivalGames Stats!");
+    }
+
+    public function execute(CommandSender $sender, $commandLabel, array $args) {
+        if(count($args) == 1 && $sender instanceof Player){
+            if($args[0] == "top"){
+                $this->plugin->getServer()->getScheduler()->scheduleAsyncTask($task = new TopStatsTask($sender->getName(), 10));
+                return;
+            }
+            if($args[0] == "mystats"){
+                $sender->sendMessage($this->plugin->getPlayerStats($sender->getName()));
+                return;
+            }
+            $sender->sendMessage("Usage: /sgstats <mystats|top>");
+            return;
+        }
+        $sender->sendMessage("Usage: /sgstats <mystats|top>");
+        return;
+    }
+}
